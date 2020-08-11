8fac49104c9e8003c4494aa08f84e0633d84b4e5
gkany <rootkangxu@yeah.net>
2020-08-11 09:11:27 +0000

    update

--- build-node/bcx_t04_transfer.js
+++ build-node/bcx_t04_transfer.js
@@ -1,59 +1,79 @@
 
 let _bcx = require('./bcx.node.js');
-var querystring = require('querystring');
 
 let bcx = _bcx.createBCX({
-    // default_ws_node:"ws://127.0.0.1:8049",
-    // ws_node_list:[
-    //      { url:"ws://127.0.0.1:8049", name:"local test" }   	
-    // ],
-    // networks:[
-    //     {
-    //         core_asset:"COCOS",
-    //         chain_id:"efe386ccc61e27c85e8dfad2946faabddc09b0d81f39b42df96414869c16fe6f" 
-    //     }
-    // ], 
-        default_ws_node:"wss://api.cocosbcx.net",
-        // default_ws_node:"ws://127.0.0.1:8049",
-        ws_node_list:[
-             {url:"wss://api.cocosbcx.net", name:"主网"}   	
-            //  {url:"ws://127.0.0.1:8049", name:"主网"}   	
-        ],
-        networks:[
-            {
-                core_asset:"COCOS",
-                chain_id:"6057d856c398875cac2650fe33caef3d5f6b403d184c5154abbff526ec1143c4" 
-                // chain_id:"efe386ccc61e27c85e8dfad2946faabddc09b0d81f39b42df96414869c16fe6f" 
-            }
-        ], 
+    default_ws_node:"ws://127.0.0.1:8049",
+    ws_node_list:[
+         { url:"ws://127.0.0.1:8049", name:"local test" }   	
+    ],
+    networks:[
+        {
+            core_asset:"COCOS",
+            chain_id:"efe386ccc61e27c85e8dfad2946faabddc09b0d81f39b42df96414869c16fe6f" 
+        }
+    ], 
     faucet_url:"",
     auto_reconnect:true,
     real_sub:true,
     check_cached_nodes_data:false                 
 });
 
-// let account_name = "jssdk-test03";
-// let pwd = "JSSDK@test000003";
+let account_name = "jssdk-test02";
+let pwd = "JSSDK@test000002";
 bcx.passwordLogin({
-    acount: account_name,
+    account: account_name,
     password: pwd
 }).then(res=>{
     console.log(account_name, pwd);
     console.info("passwordLogin res: ", res);
 
-    // if(res.code == 1) {
-    //     bcx.transferAsset({
-    //         fromAccount:"jssdk-test02",
-    //         toAccount:"init1",  //query.to,
-    //         amount:210,   //query.token,
-    //         assetId:"COCOS",
-    //         memo:"test transfer by jssdk",
-    //         isEncryption:false
-    //     }).then(result=>{
-    //         console.info('bcx transferAsset', result);
-    //     })
-    // }
+    if(res.code == 1) {
+        // bcx.transferAsset({
+        //     fromAccount:"jssdk-test02",
+        //     toAccount:"jssdk-test03",  //query.to,
+        //     amount:1,   //query.token,
+        //     assetId:"COCOS",
+        //     memo:"test transfer by jssdk, test last_block_num: " + last_block_num,
+        //     isEncryption:false
+        // }).then(result=>{
+        //     console.info('bcx transfer res: ', result);
+        // })
+
+        /////////////////////////
+        setInterval(() => {
+            var amount = Math.ceil(Math.random()*20) + 1;
+            bcx.transferAsset({
+                fromAccount:"jssdk-test02",
+                toAccount:"jssdk-test03", 
+                amount:amount,
+                assetId:"COCOS",
+                memo:"test transfer by jssdk, random: " + Math.ceil(Math.random()*210000),
+                isEncryption:false
+            }).then(result=>{
+                console.info("bcx transfer", amount, ', res: ', result);
+            })
+        }, 200);
+    }
 });
 
-
+/**
+dev@ubuntu:~/data/mrepo/JSSDK/build-node$ node bcx_t04_transfer.js 
+bcxjs version：2.2.10
+Connecting to node :  ws://127.0.0.1:8049
+Connection status :  realopen ws://127.0.0.1:8049
+synced and subscribed, chainstore ready
+jssdk-test02 JSSDK@test000002
+passwordLogin res:  { code: 1,
+  data: 
+   { account_id: '1.2.18',
+     locked: false,
+     account_name: 'jssdk-test02',
+     mode: 'account' } }
+bcx transferAsset { code: 1,
+  data: [ { fees: [Array], real_running_time: 188 } ],
+  trx_data: 
+   { trx_id: '8f56dfc5fdd3c306172afcb8b6ce2be7f706116fc9b097494ef57a082b5a5c85',
+     block_num: 145253 } }
+ 
+*/
 
